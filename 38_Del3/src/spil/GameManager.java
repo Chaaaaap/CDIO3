4fb443d8b6bb3082bafc0e4e79b3d3b99f23e771
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2015-11-20 10:45:59 +0100

    Slettet en metode der ikke bliver brugt

--- 38_Del3/src/spil/GameManager.java
+++ 38_Del3/src/spil/GameManager.java
@@ -121,21 +121,6 @@ public class GameManager
 		return true;
 	}
 
-	private void initGUI() 
-	{
-		//Creating fields for emptying them on the GUI.
-		Field[] fields = new Field[40];
-		for(int i=0; i < fields.length ; i++)
-		{
-			Field emptyField = new Empty.Builder().build();
-			fields[i] = emptyField;
-		}
-
-		GUI.create(fields);
-		GUI.showMessage("Welcome to the Money Making Dice Game\nMade by Ramyar, Mikkel, Silas, Martin and Frank - Team 38 at DTU 2015 Autumn");	
-		
-	}
-
 	private Color getChangedColor(int playerNumber)
 	{
 		switch (playerNumber)
