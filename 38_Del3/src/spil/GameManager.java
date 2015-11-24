46cb7210761f7ebfe94f7067313795a7c2c32121
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2015-11-25 00:35:02 +0100

    Har tilføjet winconditions. Kræver finjustering

--- 38_Del3/src/spil/GameManager.java
+++ 38_Del3/src/spil/GameManager.java
@@ -1,7 +1,5 @@
 package spil;
 
-import desktop_fields.Empty;
-import desktop_fields.Field;
 import desktop_resources.GUI;
 import desktop_codebehind.Car;
 import java.awt.Color;
@@ -11,12 +9,9 @@ public class GameManager
 	//Global variables of this class,
 	//which also called fields.
 	//This private fields can only be seen in this class.
-	//	private final int winnerScore = 3000;
-	private final int bankrupt = 0;
 	private Player[] players;
-	private int startingPlayer, playerCount, currentPlayerNumber, sum;
+	private int startingPlayer, playerCount, sum, bankruptCounter=0;
 	private DiceCup diceCup;
-	private boolean cointoss;
 	private GameBoard gameBoard;
 	private boolean[] bankruptPlayers;
 
@@ -24,9 +19,10 @@ public class GameManager
 	public GameManager()
 	{
 		this.diceCup = new DiceCup();
+		this.gameBoard = new GameBoard();
 	}
 
-	//Starting game handling and Rematch Handling
+	//Starting game handling and Rematch handling
 	public void manageGame()
 	{		
 		boolean wantRematch = true;
@@ -51,24 +47,26 @@ public class GameManager
 	public void startGameEngine()
 	{
 		//Setup game
-		gameBoard = new GameBoard();
 		initPlayers();
 		boolean gameIsNotWon = true;
 
-		//		Decide starting player
+		//Decide starting player
 		int i = startingPlayer;
 		//Take turns until game won
 		while (gameIsNotWon)
 		{			
 			for (;i < playerCount; i++){
+				
 				playerTurn(players[i]);
+				isPlayersBankrupt(players);
+				if(bankruptCounter == playerCount-1)
+					gameIsNotWon = false;
 
 			}
 			i=0;
 		}
 		showWinnerScreen();
 	}
-
 
 	private void showWinnerScreen() 
 	{
@@ -78,13 +76,13 @@ public class GameManager
 		//			GUI.showMessage(playerTwo.getPlayerName()+" won the game with "+playerTwo.getPlayerAccount().getBalance()+" point!!");
 	}
 
-	//
 	private void initPlayers() 
 	{	
 		String playersCountChoosenByUser = GUI.getUserButtonPressed("Please select the number of players.", "2", "3", "4", "5", "6");
 		playerCount = Integer.parseInt(playersCountChoosenByUser);
 		players = new Player[playerCount];
 		Car[] car = new Car[playerCount];
+		bankruptPlayers = new boolean[playerCount];
 
 		for(int i = 0; i < playerCount; i++)
 		{
@@ -101,13 +99,17 @@ public class GameManager
 					.build();
 			GUI.addPlayer(player.getPlayerName(), players[i].getPlayerAccount().getBalance(), car[i]);	
 			GUI.setCar(1, player.getPlayerName());
+			bankruptPlayers[i] = false;
 
 		}
 
 		GUI.getUserButtonPressed("Flip a coin to decide who starts!", "Flip Coin");
-
+		
+		//Decides who's starting
 		if(playerCount > 2) {
-			Dice dice = new Dice(playerCount,2);
+			Dice dice = new Dice(playerCount,1
+					
+					);
 			int rollResult = dice.roll();
 			startingPlayer = rollResult;
 		}
@@ -116,34 +118,39 @@ public class GameManager
 			int rollResult = dice.roll();
 			startingPlayer = rollResult;
 		}
-//				GUI.showMessage(players[currentPlayerNumber].getPlayerName() + " starts! "  + "\nLet the game begin.");
+		if(playerCount > 2)
+			GUI.showMessage(players[startingPlayer-1].getPlayerName() + " starts! "  + "\nLet the game begin.");
+		else
+			GUI.showMessage(players[startingPlayer].getPlayerName() + " starts! "+"\nLet the game begin.");
 	}
 
 	private void playerTurn(Player player) 
 	{
+		//Tests to see how many players are bankrupt
+		isPlayersBankrupt(players);
+		//Makes sure bankrupts players are skipped
 		if(player.getPlayerAccount().isBankrupt() == false) {
+			//
 			GUI.getUserButtonPressed(player.getPlayerName() + "'s turn.", "Shake Dice Cup");
 			diceCup.shake();
 			sum = diceCup.getSumResult();
 			GUI.setDice(diceCup.getDiceOne(), diceCup.getDiceTwo());
 			GUI.removeAllCars(player.getPlayerName());
-
 			player.setCurrentField((player.getCurrentField()+sum)%22);
-
-			System.out.println(player.getPlayerName()+" "+player.getCurrentField());
-
 			GUI.setCar((player.getCurrentField())+1, player.getPlayerName());
 			player.setCurrentField((player.getCurrentField()));
-			System.out.println(player.getPlayerName()+"roll: "+" "+sum);
-			System.out.println(player.getPlayerName()+" "+player.getCurrentField());
 			gameBoard.logicFields[player.getCurrentField()].landOnField(player);
-
-
-
-
 		}		
 	}
-
+	
+	private int isPlayersBankrupt(Player[] players) {
+		bankruptCounter = 0;
+		for(int i = 0; i < playerCount; i++)
+			if(players[i].getPlayerAccount().isBankrupt() == true)
+				bankruptCounter++;
+		return bankruptCounter;
+	}
+	
 	private Color getChangedColor(int playerNumber)
 	{
 		switch (playerNumber)
