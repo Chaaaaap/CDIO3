ccaf0f2b0df36a1cc7bc89e2be21fcdf7f5b65d3
Silas El-Azm Stryhn <s143599@student.dtu.dk>
2015-11-24 14:46:51 +0100

    Slettet unødvendige felt tekster

--- 38_Del3/src/spil/GameBoard.java
+++ 38_Del3/src/spil/GameBoard.java
@@ -73,46 +73,29 @@ public class GameBoard {
 			guiFields[i].setSubText(" ");
 		
 		//Initializes every field with Title and description.
-		guiFields[0].setTitle("Start"); guiFields[0].setDescription("All players start here.");
-		guiFields[1].setTitle("Tribe Encampment"); guiFields[1].setDescription("Territory\nPrice: 1000");
-		guiFields[2].setTitle("Crater"); guiFields[2].setDescription("Territory\nPrice: 1500");
-		guiFields[3].setTitle("Mountain"); guiFields[3].setDescription("Territory\nPrice: 2000");
-		guiFields[4].setTitle("Cold Desert"); guiFields[4].setDescription("Territory\nPrice: 3000");
-		guiFields[5].setTitle("Black Cave"); guiFields[5].setDescription("Territory\nPrice: 4000");
-		guiFields[6].setTitle("The Werewall"); guiFields[6].setDescription("Territory\nPrice: 4300");
-		guiFields[7].setTitle("Mountain Village"); guiFields[7].setDescription("Territory\nPrice: 4750");
-		guiFields[8].setTitle("South Citadel"); guiFields[8].setDescription("Territory\nPrice: 5000");
-		guiFields[9].setTitle("Palace gates"); guiFields[9].setDescription("Territory\nPrice: 5500");
-		guiFields[10].setTitle("Tower"); guiFields[10].setDescription("Territory\nPrice: 6000");
-		guiFields[11].setTitle("Castle"); guiFields[11].setDescription("Territory\nPrice: 8000");
-		guiFields[12].setTitle("Walled City"); guiFields[12].setDescription("Refuge\nYou receive 5000");
-		guiFields[13].setTitle("Monastery"); guiFields[13].setDescription("Refuge\nYou receive 500");
-		guiFields[14].setTitle("Huts in the mountain"); guiFields[14].setDescription("Labor camp\nPrice: 2500");
-		guiFields[15].setTitle("The pit"); guiFields[15].setDescription("Labor camp\nPrice: 2500");
-		guiFields[16].setTitle("Goldmine"); guiFields[16].setDescription("Tax\nChoose between paying 2000 or 10% of your total assets");
-		guiFields[17].setTitle("Caravan"); guiFields[17].setDescription("Tax\nChoose between paying 4000 or 10% of your total assets");
-		guiFields[18].setTitle("Second Sail"); guiFields[18].setDescription("Fleet\nPrice: 4000");
-		guiFields[19].setTitle("Sea Grover"); guiFields[19].setDescription("Fleet\nPrice: 4000");
-		guiFields[20].setTitle("The Buccaneers"); guiFields[20].setDescription("Fleet\nPrice: 4000");
-		guiFields[21].setTitle("Privateer armade"); guiFields[21].setDescription("Fleet\nPrice: 4000");
-		guiFields[22].setTitle("Tribe Encampment"); guiFields[22].setDescription("Territory\nPrice: 1000");
-		guiFields[23].setTitle("Crater"); guiFields[23].setDescription("Territory\nPrice: 1500");
-		guiFields[24].setTitle("Mountain"); guiFields[24].setDescription("Territory\nPrice: 2000");
-		guiFields[25].setTitle("Cold Desert"); guiFields[25].setDescription("Territory\nPrice: 3000");
-		guiFields[26].setTitle("Black Cave"); guiFields[26].setDescription("Territory\nPrice: 4000");
-		guiFields[27].setTitle("The Werewall"); guiFields[27].setDescription("Territory\nPrice: 4300");
-		guiFields[28].setTitle("Mountain Village"); guiFields[28].setDescription("Territory\nPrice: 4750");
-		guiFields[29].setTitle("South Citadel"); guiFields[29].setDescription("Territory\nPrice: 5000");
-		guiFields[30].setTitle("Palace gates"); guiFields[30].setDescription("Territory\nPrice: 5500");
-		guiFields[31].setTitle("Tower"); guiFields[31].setDescription("Territory\nPrice: 6000");
-		guiFields[32].setTitle("Castle"); guiFields[32].setDescription("Territory\nPrice: 8000");
-		guiFields[33].setTitle("Walled City"); guiFields[33].setDescription("Refuge\nYou receive 5000");
-		guiFields[34].setTitle("Monastery"); guiFields[34].setDescription("Refuge\nYou receive 500");
-		guiFields[35].setTitle("Huts in the mountain"); guiFields[35].setDescription("Labor camp\nPrice: 2500");
-		guiFields[36].setTitle("The pit"); guiFields[36].setDescription("Labor camp\nPrice: 2500");
-		guiFields[37].setTitle("Goldmine"); guiFields[37].setDescription("Tax\nChoose between paying 2000 or 10% of your total assets");
-		guiFields[38].setTitle("Caravan"); guiFields[38].setDescription("Tax\nChoose between paying 4000 or 10% of your total assets");
-		guiFields[39].setTitle("Second Sail"); guiFields[39].setDescription("Fleet\nPrice: 4000");
+		guiFields[0].setTitle("Start"); guiFields[0].setDescription("All players start here."); guiFields[0].setSubText("Start");
+		guiFields[1].setTitle("Tribe Encampment"); guiFields[1].setDescription("Territory\nPrice: 1000"); guiFields[1].setSubText("Territory");
+		guiFields[2].setTitle("Crater"); guiFields[2].setDescription("Territory\nPrice: 1500"); guiFields[2].setSubText("Territory");
+		guiFields[3].setTitle("Mountain"); guiFields[3].setDescription("Territory\nPrice: 2000"); guiFields[3].setSubText("Territory");
+		guiFields[4].setTitle("Cold Desert"); guiFields[4].setDescription("Territory\nPrice: 3000"); guiFields[4].setSubText("Territory");
+		guiFields[5].setTitle("Black Cave"); guiFields[5].setDescription("Territory\nPrice: 4000"); guiFields[5].setSubText("Territory");
+		guiFields[6].setTitle("The Werewall"); guiFields[6].setDescription("Territory\nPrice: 4300"); guiFields[6].setSubText("Territory");
+		guiFields[7].setTitle("Mountain Village"); guiFields[7].setDescription("Territory\nPrice: 4750"); guiFields[7].setSubText("Territory");
+		guiFields[8].setTitle("South Citadel"); guiFields[8].setDescription("Territory\nPrice: 5000"); guiFields[8].setSubText("Territory");
+		guiFields[9].setTitle("Palace gates"); guiFields[9].setDescription("Territory\nPrice: 5500"); guiFields[9].setSubText("Territory");
+		guiFields[10].setTitle("Tower"); guiFields[10].setDescription("Territory\nPrice: 6000"); guiFields[10].setSubText("Territory");
+		guiFields[11].setTitle("Castle"); guiFields[11].setDescription("Territory\nPrice: 8000"); guiFields[11].setSubText("Territory");
+		guiFields[12].setTitle("Walled City"); guiFields[12].setDescription("Refuge\nYou receive 5000"); guiFields[12].setSubText("Refuge");
+		guiFields[13].setTitle("Monastery"); guiFields[13].setDescription("Refuge\nYou receive 500"); guiFields[13].setSubText("Refuge");
+		guiFields[14].setTitle("Huts in the mountain"); guiFields[14].setDescription("Labor camp\nPrice: 2500"); guiFields[14].setSubText("Labor Camp");
+		guiFields[15].setTitle("The pit"); guiFields[15].setDescription("Labor camp\nPrice: 2500"); guiFields[15].setSubText("Labor Camp");
+		guiFields[16].setTitle("Goldmine"); guiFields[16].setDescription("Tax\nChoose between paying 2000 or 10% of your total assets"); guiFields[16].setSubText("Tax");
+		guiFields[17].setTitle("Caravan"); guiFields[17].setDescription("Tax\nChoose between paying 4000 or 10% of your total assets"); guiFields[17].setSubText("Tax");
+		guiFields[18].setTitle("Second Sail"); guiFields[18].setDescription("Fleet\nPrice: 4000"); guiFields[18].setSubText("Fleet");
+		guiFields[19].setTitle("Sea Grover"); guiFields[19].setDescription("Fleet\nPrice: 4000"); guiFields[19].setSubText("Fleet");
+		guiFields[20].setTitle("The Buccaneers"); guiFields[20].setDescription("Fleet\nPrice: 4000"); guiFields[20].setSubText("Fleet");
+		guiFields[21].setTitle("Privateer armade"); guiFields[21].setDescription("Fleet\nPrice: 4000"); guiFields[21].setSubText("Fleet");
+
 	}
 	
 	private void initGUI() 
