6754795ee948f162b16edae4568a39413db99df7
Martin <Martin@10.16.97.134>
2015-11-26 13:05:28 +0100

    Har kommenteret PlayerAccount klassen

--- 38_Del3/src/spil/PlayerAccount.java
+++ 38_Del3/src/spil/PlayerAccount.java
@@ -2,8 +2,11 @@ package spil;
 
 public class PlayerAccount {
 
+	//Attribut of the class.
 	private int balance;
 
+	//The contructor of PlayerAccount, which sets the the balance 
+	//for the player.
 	public PlayerAccount(int balance) 
 	{
 		if(balance < 0)
@@ -12,6 +15,7 @@ public class PlayerAccount {
 			this.balance = balance;
 	}
 
+	//This method shows whether the player is bankrupt or not.
 	public boolean isBankrupt() {
 		if(this.balance <= 0)
 			return true;
@@ -19,7 +23,9 @@ public class PlayerAccount {
 			return false;
 	}
 
-
+	//This method adjust the balance of the player.
+	//if the players balance is lower than zero, then it's 
+	//changed to zero, and the player is bankrupt.
 	public void adjustBalance(int amount) 
 	{
 		if((this.balance + amount)<0)
@@ -28,6 +34,7 @@ public class PlayerAccount {
 			this.balance = this.balance + amount;
 	}
 
+	//This method sets the balance of the players starting account.
 	public void setBalance(int amount) 
 	{
 		if(amount < 0)
@@ -41,6 +48,7 @@ public class PlayerAccount {
 		return this.balance;
 	}
 	
+	//This method tranfers from one player to another.
 	public void transfer(PlayerAccount playerAccount, int amount) {
 		adjustBalance(-amount);
 		playerAccount.adjustBalance(amount);
