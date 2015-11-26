550d9ab2d441bba7ae35c9f6a1ab8d9bc01f6390
Martin <Martin@10.16.97.134>
2015-11-26 12:26:59 +0100

    Har kommenteret Ownable klassen

--- 38_Del3/src/spil/Ownable.java
+++ 38_Del3/src/spil/Ownable.java
@@ -2,6 +2,9 @@ package spil;
 
 public abstract class Ownable extends Felt {
 	
+	//This is an abstract class that extends from the abstract Felt class.
+	//the class is abstract because other classes have to extend
+	//from this class. Its the body of all classes, that are ownable.
 	private Player owner;
 	private int pris;
 
@@ -10,6 +13,8 @@ public abstract class Ownable extends Felt {
 		this.pris = pris;
 	}
 	
+	//Abstract methods. 
+	//These methods are being overrided in the subclasses.
 	public abstract int getRent(Player player);
 	
 	public abstract void setOwner(Player player);
