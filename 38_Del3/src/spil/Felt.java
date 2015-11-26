6994469f2defd0a138490041fc42190fadfbd8a9
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2015-11-26 11:32:45 +0100

    Tilføjet lidt kommentarer

--- 38_Del3/src/spil/Felt.java
+++ 38_Del3/src/spil/Felt.java
@@ -1,11 +1,14 @@
 package spil;
 
+
 public abstract class Felt {
 	
 	public Felt() {
 		
 	}
 	
+	//These methods are abstract because we don't ever use them from the Felt class.
+	//They will always overriden from the subclasses of Felt.
 	public abstract void landOnField(Player player);
 	public abstract String getFeltBesked(Player player);
 	
