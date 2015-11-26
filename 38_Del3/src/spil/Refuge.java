675e23395a4e7d1af111cf57db20e0575083eb20
Martin <Martin@10.16.97.134>
2015-11-26 12:52:38 +0100

    Ændret metoden pay til bonus

--- 38_Del3/src/spil/Refuge.java
+++ 38_Del3/src/spil/Refuge.java
@@ -4,28 +4,36 @@ import desktop_resources.GUI;
 
 public class Refuge extends Felt {
 	
+	//Global variables of this class,
+	//which also called fields.
+	//This private fields can only be seen in this class.
 	private int bonus;
 	private Player player;
 	private String feltNavn;
 	
+	//The Refuge constructor takes two parameters, bonus and feltNavn.
 	public Refuge(int bonus, String feltNavn) {
 		this.bonus = bonus;
 		this.feltNavn = feltNavn;
 	}
 
+	//The landOnField method shows the getFeltBesked in the GUI,
+	// and add the bonus by the bonus metod.
 	@Override
 	public void landOnField(Player player) {
 		this.player = player;
 		GUI.showMessage(getFeltBesked(player));
-		pay();
+		bonus();
 	}
 
+	//Makes the string that is showed in the GUI.
 	@Override
 	public String getFeltBesked(Player player) {
 		return player.getPlayerName()+", you landed on "+feltNavn+"\nYou receive "+bonus+"$";
 	}
 	
-	private void pay() {
+	//The bonus method add the Refuge fields bonus to the given players account.
+	private void bonus() {
 		player.getPlayerAccount().adjustBalance(bonus);
 		GUI.setBalance(player.getPlayerName(), player.getPlayerAccount().getBalance());
 	}
