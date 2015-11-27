3719f320520a09fb8491bb81d6ed1a5f92b7f2e9
Martin <Martin@10.16.97.134>
2015-11-27 11:08:03 +0100

    Lavet TestCase 2 og 3

---
+++ 38_Del3/src/test/TestCase3.java
@@ -0,0 +1,44 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+import spil.*;
+
+public class TestCase3 {
+	
+	private Player player;
+	private Ownable Fleet4000;
+	
+
+	@Before
+	public void setUp() throws Exception {
+		player = new Player();
+		player.getPlayerAccount().setBalance(30000);
+		player.setPlayerName("Player");
+		Fleet4000 = new Fleet(4000, "Fleet 4000");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		//?
+	}
+
+	@Test
+	public void test() {
+		int playerExpected = 30000;
+		int playerActual = player.getPlayerAccount().getBalance();
+		Assert.assertEquals(playerExpected, playerActual);
+		
+		Fleet4000.buyFieldOption(player);
+		
+		playerExpected = 30000 - 4000;
+		playerActual = player.getPlayerAccount().getBalance();
+		Assert.assertEquals(playerExpected, playerActual);
+	}
+
+}
